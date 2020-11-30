284bd2540ef8549b6d1da6840ab1b7c448ca8f41
Ari Paasonen <paasar@gmail.com>
2020-11-30 18:55:39 +0200

    Improved algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 extern crate image;
 
 use image::{GenericImageView};
+use std::cmp::{max, min};
 
-const GREEN_SCALE: ([u8;3], [u8;3]) = ([0, 80, 0], [100, 255, 100]);
+//TODO Other ushabti colors
+const GREEN_SCALE: ([u8;3], [u8;3]) = ([0, 80, 0], [120, 255, 120]);
+const PIXEL_SURROUND_RANGE: u32 = 10;
+const PIXEL_SURROUND_AREA: u32 = PIXEL_SURROUND_RANGE * PIXEL_SURROUND_RANGE;
 
 fn is_within(values: [u8;3], acceptable_range: ([u8;3], [u8;3])) -> bool {
     let [r, g, b] = values;
@@ -19,25 +23,49 @@ fn main() {
 
     let img = image::open("ushabti_1_small.jpg").unwrap();
 
-    // The dimensions method returns the images width and height.
     let dim= img.dimensions();
     let (width, height) = dim;
-    println!("dimensions {:?}", dim);
+    println!("Dimensions {:?}, ColorType {:?}", dim, img.color());
 
-    // The color method returns the image's `ColorType`.
-    println!("{:?}", img.color());
+    let mut result_img_buf = image::ImageBuffer::new(width, height);
 
-    let mut imgbuf = image::ImageBuffer::new(width, height);
+    let mut color_array: Vec<Vec<[u8; 3]>> = vec![vec![[0,0,0]; height as usize]; width as usize];
 
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let original_pixel = img.get_pixel(x, y);
-        let image::Rgba(data) = original_pixel;
-        if is_within([data[0], data[1], data[2]], GREEN_SCALE) {
-            *pixel = image::Rgb([data[0], data[1], data[2]])
+    // TODO I'd like to enumerate the actual image
+    for (x, y, _) in result_img_buf.enumerate_pixels() {
+        let image::Rgba(data) = img.get_pixel(x, y);
+        color_array[x as usize][y as usize] = [data[0], data[1], data[2]]
+    }
+
+    for (x, y, pixel) in result_img_buf.enumerate_pixels_mut() {
+        // This is a bit unsafe, but I expect to handle smaller images than width/height of i32::MAX.
+        let min_x = max(x as i32 - PIXEL_SURROUND_RANGE as i32, 0) as u32;
+        let max_x = min(x + PIXEL_SURROUND_RANGE, width);
+        let min_y = max(y as i32 - PIXEL_SURROUND_RANGE as i32, 0) as u32;
+        let max_y = min(y + PIXEL_SURROUND_RANGE, height);
+
+        // TODO Slow?
+        let mut surrounding_pixels = Vec::new();
+        for sx in min_x..max_x {
+            for sy in min_y..max_y {
+                surrounding_pixels.push(color_array[sx as usize][sy as usize]);
+            }
+        }
+
+        let mut within = 0;
+        let original_rgb = color_array[x as usize][y as usize];
+        for px in surrounding_pixels {
+            if is_within(px, GREEN_SCALE) {
+                within += 1;
+            }
+        }
+
+        if within as f64 / PIXEL_SURROUND_AREA as f64 > 0.2 {
+            *pixel = image::Rgb(original_rgb)
         } else {
             *pixel = image::Rgb([0 as u8, 0 as u8, 0 as u8]);
         }
     }
 
-    imgbuf.save("output.png").unwrap();
+    result_img_buf.save("output.png").unwrap();
 }
